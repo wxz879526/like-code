542583606e24100648f6e590ab2c1ebda54d0081
wxz879526 <wxz879526@sina.com>
2020-10-12 22:23:11 +0800

    update

--- network/linux/client/main.cpp
+++ network/linux/client/main.cpp
@@ -9,45 +9,36 @@
 void error_handler(char *message);
 
 int main(int argc, char* argv[]) {
-    int serv_sock;
-    int client_sock;
+    int sock_fd;
 
     struct sockaddr_in serv_addr;
-    struct sockaddr_in client_addr;
 
-    char message[] = "Hello World!";
-
-    if (argc != 2)
+    if (argc != 3)
     {
-        printf("Usage: %s <port>\n", argv[0]);
+        printf("Usage: %s <ip> <port>\n", argv[0]);
         exit(1);
     }
 
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if (serv_sock == -1)
+    sock_fd = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock_fd == -1)
         error_handler("socket error");
 
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(atoi(argv[1]));
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
 
-    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-        error_handler("bind error");
+    if (connect(sock_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handler("connect() error");
 
-    printf("server start listen...........\n");
+    char message[100]{0};
+    int str_len = read(sock_fd, message, 100);
+    if (str_len == -1)
+        error_handler("read() error");
 
-    if (listen(serv_sock, 5) == -1)
-        error_handler("listen error");
+    printf("message from server: %s\n", message);
 
-    socklen_t client_len = sizeof(client_addr);
-    client_sock = accept(serv_sock, (struct sockaddr*)&client_addr, &client_len);
-    if (client_sock == -1)
-        error_handler("accept error");
-
-    write(client_sock, message, sizeof(message));
-    close(client_sock);
-    close(serv_sock);
+    close(sock_fd);
 
     return 0;
 }
